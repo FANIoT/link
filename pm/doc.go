f6c4eee4f7b8d4eb6accb4a0e05bcff9a0040e9b
Parham Alvani <parham.alvani@gmail.com>
2018-09-20 21:01:27 +0430

    Adds pm package.

---
+++ pm/doc.go
@@ -0,0 +1,16 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 20-09-2018
+ * |
+ * | File Name:     doc.go
+ * +===============================================
+ */
+
+// Package pm accesses pm component database and fetches and then caches
+// things information.
+package pm
