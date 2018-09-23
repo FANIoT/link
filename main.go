3f50b5daeceedc83210f2ea72ab6d4573cce864e
Parham Alvani <parham.alvani@gmail.com>
2018-09-24 00:39:07 +0330

    Adds buffalo service into main.

--- main.go
+++ main.go
@@ -16,12 +16,21 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/I1820/link/actions"
 	"github.com/I1820/link/mqtt"
 )
 
 func main() {
 	fmt.Println("18.20 at Sep 07 2016 7:20 IR721")
 
+	// buffalo http service
+	go func() {
+		app := actions.App()
+		if err := app.Serve(); err != nil {
+			log.Fatalf("Buffalo Service failed with %s", err)
+		}
+	}()
+	// non-http services
 	if err := mqtt.New().Run(); err != nil {
 		log.Fatalf("MQTT Service failed with %s", err)
 	}
