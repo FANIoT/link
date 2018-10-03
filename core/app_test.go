d24cf6a25f0be2586bb509030f9399ce0ebdc1db
Parham Alvani <parham.alvani@gmail.com>
2018-10-03 22:15:40 +0330

    Corrects test and benchmark.

--- core/app_test.go
+++ core/app_test.go
@@ -34,25 +34,27 @@ func TestPipeline(t *testing.T) {
 	a.Run()
 	ts := time.Now()
 
-	a.Data(types.State{
+	assert.NoError(t, a.Data(types.State{
+		Raw:     18.20,
 		At:      ts,
 		Asset:   aName,
 		ThingID: tID,
 		Project: pName,
-	})
+	}))
 	// wait until data traverse pipeline
-	time.Sleep(1 * time.Second)
+	time.Sleep(2 * time.Second)
 
 	var d types.State
 	q := a.db.Collection(fmt.Sprintf("data.%s.%s", pName, tID)).FindOne(context.Background(), bson.NewDocument(
-		bson.EC.SubDocument("timestamp", bson.NewDocument(
+		bson.EC.SubDocument("at", bson.NewDocument(
 			bson.EC.Time("$gte", ts),
 		)),
-		bson.EC.String("thingid", "el-thing"),
+		bson.EC.String("asset", aName),
 	))
 	assert.NoError(t, q.Decode(&d))
 
 	assert.Equal(t, d.At.Unix(), ts.Unix())
+	assert.Equal(t, 18.20, d.Value.Number)
 }
 
 func BenchmarkPipeline(b *testing.B) {
@@ -61,18 +63,20 @@ func BenchmarkPipeline(b *testing.B) {
 
 	wait := make(chan struct{})
 	a.cli.Subscribe(fmt.Sprintf("i1820/projects/%s/things/%s/assets/%s/state", pName, tID, aName), 0, func(client paho.Client, message paho.Message) {
+		fmt.Println("Hello")
 		wait <- struct{}{}
 	})
 
 	for i := 0; i < b.N; i++ {
 		ts := time.Now()
 
-		a.Data(types.State{
+		assert.NoError(b, a.Data(types.State{
 			At:      ts,
+			Raw:     18.20,
 			Asset:   aName,
 			ThingID: tID,
 			Project: pName,
-		})
+		}))
 
 		<-wait
 	}
